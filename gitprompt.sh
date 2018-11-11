30fbb4fe8d387c200eabe15e6a1d22249eeb17c0
Christer Enfors <christer.enfors@gmail.com>
2018-11-11 18:09:33 +0100

    Insert ansicolor.sh in gitprompt.sh
    Include the contents of ansicolor.sh inside gitprompt.sh, making
    ansicolor.sh superfluous.

--- gitprompt.sh
+++ gitprompt.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-gs_prefix=.
-. $gs_prefix/ansicolor.sh
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+RESET="\033[0m"
 
 # Display the exit status of the previous command, if non-zero.
 function ExitStatus
